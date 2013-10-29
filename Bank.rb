22850f8bbdca014f654ee6580e36ca2b52621c0f
larrykas <larrykas@users.noreply.github.com>
2013-10-29 09:35:54 +0300

    Update Bank.rb

--- Bank.rb
+++ Bank.rb
@@ -28,3 +28,40 @@ def initialize(balance, name, account_number)
  return @balance
  end
  end
+#######################################################################################################
+ 
+Larry$ irb
+irb(main):001:0> require './Bank.rb'
+=> true
+irb(main):002:0> a = Account.new(0, "Steve", 011234)
+=> #<Account:0x007fc04481e280 @balance=0, @name="Steve", @account_number=4764>
+irb(main):003:0> a.balance
+Name: Steve
+Account number: 4764
+Balance: 0
+=> nil
+irb(main):004:0> a.deposit(100)
+=> 100
+irb(main):006:0> a.withdraw(25)
+=> 75
+irb(main):007:0> b = Account.new(4000, "Jeron", 011235)
+=> #<Account:0x007fc044851338 @balance=4000, @name="Jeron", @account_number=4765>
+irb(main):008:0> b.balance
+Name: Jeron
+Account number: 4765
+Balance: 4000
+=> nil
+irb(main):009:0> puts b.balance
+Name: Jeron
+Account number: 4765
+Balance: 4000
+
+=> nil
+irb(main):010:0> a.transfer(50, b)
+=> 4050
+irb(main):011:0> b.balance
+Name: Jeron
+Account number: 4765
+Balance: 4050
+
+#######################################################################################################
